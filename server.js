228f6e293db2289dba34f485130e69443afa6681
seydou ba <baseydou95@gmail.com>
2022-03-22 17:30:21 -0400

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const app = express();
 //create the middleware
 app.use(express.json());
 app.use(cors());
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use("/", contactRoute);
 
 if (process.env.NODE_ENV === "production") {
